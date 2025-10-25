cf3b4be6e5d44c5cedaecd3812f018a98063fbbf
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2025-10-25 22:41:52 +0330

    add some details to voters

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def create_pg_dbs():
         """
         CREATE TABLE IF NOT EXISTS candidates (
             candidate_id SERIAL PRIMARY KEY,
-            candidate_name VARCHAR(255)
+            candidate_name VARCHAR(255) NOT NULL
         )
     """
     )
@@ -23,7 +23,9 @@ def create_pg_dbs():
         """
             CREATE TABLE IF NOT EXISTS voters (
                 user_id SERIAL PRIMARY KEY,
-                user_name VARCHAR(255)
+                user_name VARCHAR(255),
+                national_code CHAR(10) CHECK (national_code ~ '^[0-9]{10}$'),
+                phone CHAR(11) CHECK (phone ~ '^0[0-9]{10}$')
             )
         """
     )
@@ -32,8 +34,8 @@ def create_pg_dbs():
         """
         CREATE TABLE IF NOT EXISTS voters_history (
         vote_id SERIAL PRIMARY KEY,
-        user_id INTEGER,
-        candidate_id INTEGER,
+        user_id INTEGER REFERENCES voters(user_id),
+        candidate_id INTEGER REFERENCES candidates(candidate_id),
         ts  TIMESTAMP
         )
     """
@@ -53,12 +55,12 @@ if __name__ == "__main__":
     create_pg_dbs()
     cur.execute("SELECT * FROM voters")
     users = cur.fetchall()
-    print(users)
     for c in CANDIDATES:
-        print(c)
         cur.execute("INSERT INTO candidates(candidate_name) VALUES(%s)", (c,))
 
     for i in range(1, NUM_USERS + 1):
-        cur.execute("INSERT INTO voters(user_name) VALUES(%s)", (f"usser_{i}",))
+        national_code = f'012345{i:04d}'
+        phone_number = f'0901234{i:04d}'
+        cur.execute("INSERT INTO voters(user_name, national_code, phone) VALUES(%s, %s, %s)", (f"user_{i}", national_code, phone_number))
 
     conn.commit()
